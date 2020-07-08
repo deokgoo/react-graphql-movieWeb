af2eec64493534abc4ff5fd721479a561df5cde0
deokgoo <kkddgg1001@gmail.com>
2020-07-08 11:27:03 +0900

    initial

---
+++ build/precache-manifest.fc550c2bc8a35993dbadce8e1b9c46eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50bcfe923f2ed8d4a9dd500b972ad411",
+    "url": "/react-graphql-movieWeb/index.html"
+  },
+  {
+    "revision": "2cf5acb04ae19d987893",
+    "url": "/react-graphql-movieWeb/static/js/2.cc80a059.chunk.js"
+  },
+  {
+    "revision": "da42135372d3365a7233548ad1382c4d",
+    "url": "/react-graphql-movieWeb/static/js/2.cc80a059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df3a44935915667feb12",
+    "url": "/react-graphql-movieWeb/static/js/main.94471c14.chunk.js"
+  },
+  {
+    "revision": "395e3172845af094d1f5",
+    "url": "/react-graphql-movieWeb/static/js/runtime-main.7ef359f1.js"
+  }
+]);
